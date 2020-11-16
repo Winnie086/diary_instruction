6badaffedd588a2379ee25cdb46bfec52b23c6e6
Winnie <0433034@nkust.edu.tw>
2020-11-17 01:25:22 +0800

    簡易會員註冊登入管理系統樣板

--- forget_pw.php
+++ forget_pw.php
@@ -8,35 +8,34 @@
 <body>
 
 <?php
-if(isset($_POST['email'])){
-    $dsn="mysql:host=localhost;dbname=member;charset=utf8";
-    $pdo=new PDO($dsn,'root','');
-    $sql="select *from login where email='{$_POST['email']}'";
-    echo $sql;
-    $chk=$pdo->query($sql)->fetch();
-    echo "<pre>";
-    print_r($chk);
-    echo "</pre>";
-    if(!empty($chk)){
-        $res=$chk['pw'];
-    }else{
-        $res="沒這人";
-    }
+// if(isset($_POST['email'])){
+//     $dsn="mysql:host=localhost;dbname=member;charset=utf8";
+//     $pdo=new PDO($dsn,'root','');
+//     $sql="select *from login where email='{$_POST['email']}'";
+//     echo $sql;
+//     $chk=$pdo->query($sql)->fetch();
+//     echo "<pre>";
+//     print_r($chk);
+//     echo "</pre>";
+//     if(!empty($chk)){
+//         $res=$chk['pw'];
+//     }else{
+//         $res="沒這人";
+//     }
 
-}
+// }
 ?>
 
-<form action="?" method="post">
+<!-- <form action="?" method="post">
 <input type="text" name="emal">
 <input type="submit" name="查詢">
-</form> 
-<span>
+</form>  -->
+
 <?php
-if(isset($res)){
-    echo $res;
-} 
+// if(isset($res)){
+//     echo $res;
+// } 
 ?>
-</span>
 
 
 </body>
